698288336fa923868790937e66dba6c0671a3c0e
Tim Schreiber <timschreiber@users.noreply.github.com>
2017-09-25 17:18:26 -0400

    Initial Commit

---
+++ BundlingSpike.Core/Entities/File.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BundlingSpike.Core.Entities
+{
+    public class File
+    {
+        public Guid FileId { get; set; }
+        public Guid BundleId { get; set; }
+        public string FileName { get; set; }
+        public string Source { get; set; }
+        public int Order { get; set; }
+
+        public virtual Bundle Bundle { get; set; }
+
+        public string Minified
+        {
+            get
+            {
+                var minifier = new Minifier();
+                return Bundle.Type == BundleType.StyleSheet
+                    ? minifier.MinifyStyleSheet(Source)
+                    : minifier.MinifyJavaScript(Source);
+            }
+        }
+    }
+}
---
+++ BundlingSpike.Core/Entities/Site.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BundlingSpike.Core.Entities
+{
+    public class Site
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<Bundle> Bundles { get; set; } = new HashSet<Bundle>();
+    }
+}
